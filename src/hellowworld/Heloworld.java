286c03632dd6de27fca1b42802b8425a38e27e59
SagarMagdum <s.b.magadum333@gmail.com>
2021-12-26 12:09:58 +0530

    new class added

---
+++ src/hellowworld/Heloworld.java
@@ -0,0 +1,12 @@
+package hellowworld;
+
+public class Heloworld {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		System.out.println("Helloworld");
+	}
+
+}
